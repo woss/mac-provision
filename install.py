c0b17675e5d9f12a0ea900d1bb894500ccc03f9d
Danijel Maricic <danijel@sevensignals.io>
2016-07-20 23:02:53 +0300

    init

---
+++ install.py
@@ -0,0 +1,23 @@
+#!/System/Library/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python
+
+import os
+import distutils.spawn
+
+system_packages = ['wget','cask']
+
+def is_tool(name):
+    return distutils.spawn.find_executable(name) is not None
+
+if not is_tool('brew'):
+    print 'Installing Homebrew'
+    os.system('/usr/bini/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"')
+else:
+    print 'Homebrew installed'
+
+
+if not is_tool('salt'):
+    print 'Installing saltstack...'
+    os.system('brew install saltstack')
+else:
+    print 'Saltstack installed'
+
